ab2ef16e23c036a3fd3e3bdb17d47b207727df22
notwn <nandiinewt@gmail.com>
2024-10-09 10:16:46 +0300

    comment added

--- pinger.py
+++ pinger.py
@@ -6,7 +6,7 @@ ip ="127.0.0.1"
 current_os= platform.system().lower()
 if current_os == "windows":
 #building our ping commands to windows
-    ping_cmd= f"ping -n 1 -w 2 {ip} > nul"
+    ping_cmd= f"ping -n 1 -w 2 {ip} > null"
 else:
     ping_cmd= f"ping -c 1 -w 2 {ip}> /dev/null 2>&1"
 # Execute command and capture exit code
