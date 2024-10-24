53251918396e0040e717b75424a047c650f64bb6
notwn <nandiinewt@gmail.com>
2024-10-24 12:02:21 +0300

    new file

---
+++ hackme.py
@@ -0,0 +1,24 @@
+#create a function to collect user information
+def create_user_info():
+    user_info = {
+        "name": input("What is your name? "),
+        "favorite_color": input("What is your favorite color? "),
+        "first_pet_name": input("What was your first pet's name? "),
+        "mother_maiden_name": input("What is your mother's maiden name? "),
+        "elementary_school": input("What elementary school did you attend? ")
+    }
+    return user_info
+
+#Saving user information to a file by creating a function
+def save_user_info(user_info):
+    with open("hackme.txt", "w") as file:
+        for key, value in user_info.items():
+            file.write(f"{key.capitalize().replace('_', ' ')}: {value}\n")
+
+# Main program
+def main():
+    user_info = create_user_info()
+    save_user_info(user_info)
+
+if __name__ == "__main__":
+    main()
